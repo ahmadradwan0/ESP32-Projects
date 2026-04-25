58b21990cb52ace47fa2da780db49d4ccf4ae41f
Ahmad Radwan <ahmad@ahmadradwan.dev>
2026-04-25 09:04:14 -0400

    Clean Up

--- lib/UI/Screen.cpp
+++ lib/UI/Screen.cpp
@@ -8,45 +8,51 @@
 
 Screen::Screen() : _display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 {
-
 }
 
-bool Screen::init()
+bool Screen::Init()
 {
     Wire.begin(8, 9);
 
-    if (!_display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    if (!_display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR))
+    {
         return false;
     }
 
     _display.setTextColor(COLOR_ON);
-    _display.setTextWrap(true);
-    clear();
+    _display.setTextWrap(false);
+    Clear();
     return true;
 }
 
-void Screen::clear()
+void Screen::Clear()
 {
     _display.clearDisplay();
-    _display.display();
 }
 
-void Screen::showText(String msg, int size)
+void Screen::ShowText(String msg, int x, int y, int size)
 {
-    _display.clearDisplay();
     _display.setTextSize(size);
-    _display.setCursor(0, 0);
-    _display.println(msg);
-    _display.display();
+    _display.setCursor(x, y);
+    _display.print(msg);
 }
 
-void Screen::showTwoLine(String line1, String line2) {
-    _display.clearDisplay();
+void Screen::ShowTwoLine(String line1, String line2)
+{
     _display.setTextSize(1);
     _display.setCursor(0, 0);
     _display.println(line1);
     _display.setTextSize(2);
     _display.setCursor(0, 16);
     _display.println(line2);
+}
+
+void Screen::Show()
+{
     _display.display();
+}
+
+int Screen::GetTextPixelWidth(String text, int size)
+{
+    return text.length() * 6 * size;
 }
--- lib/UI/Screen.h
+++ lib/UI/Screen.h
@@ -9,10 +9,12 @@ class Screen
         static const uint16_t COLOR_OFF = SSD1306_BLACK;
 
         Screen();
-        bool init();
-        void clear();
-        void showText(String msg, int size = 2);
-        void showTwoLine(String line1, String line2);
+        bool Init();
+        void Clear();
+        void Screen::ShowText(String msg,int x, int y ,int size);
+        void ShowTwoLine(String line1, String line2);
+        void Screen::Show();
+        int Screen::GetTextPixelWidth(String text, int size);
 
     private:
         Adafruit_SSD1306 _display;
